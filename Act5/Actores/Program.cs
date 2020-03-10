84b5d6c52d86dbd9bcfc186e58ab083dd1963716
ErnestoVazquez94 <60420316+ErnestoVazquez94@users.noreply.github.com>
2020-03-10 00:24:54 -0700

    a

---
+++ Act5/Actores/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+ 
+namespace Actores
+{
+    class Actores
+    {
+        private string Nombre;
+        public Actores(string N)
+        {
+            Nombre = N;
+        }
+        public void imprime()
+        {
+            Console.WriteLine(Nombre);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Actores> Actor = new List<Actores>();
+            Actor.Add(new Actores("Leonardo Dicaprio"));
+            Actor.Add(new Actores("Johnny Depp"));
+            Actor.Add(new Actores("Vin Diesel"));
+            Actor.Add(new Actores("Will Smith"));
+ 
+            foreach(Actores a in Actor)
+            {
+                a.imprime();
+            }
+            
+        }
+    }
+}
